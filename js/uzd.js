47f9f1f1701f9ff63cad5a3dad1d2a7998fef78b
Marius <marius@example.com>
2022-07-22 15:23:57 +0300

    uzd

---
+++ js/uzd.js
@@ -0,0 +1,17 @@
+const nums = [-1, 5, 8, -20, 10, 12, -5, 14];
+
+// naudojan array.forEach()
+// 1. atspausdinti html paragrafe visus skaicius  <p id="para"></p>
+// 2. atspausdinti html h2 elemente visus padalintus is 10 - <h2 id="h2"></h2>
+// 3. gauti ir atspausdinti nauja masyva kuriame visos reiksmes butu padaugintos is 3
+// 4. gauti ir atspausdinti nauja masyva kuriame visos reiksmes padaugintos is ju indexo.
+// 5. grazinti nauja masyva kuriame butu reikmes didesnes uz 9
+// 6.  grazinti ir atspausdinti consoleje visas neigiamas reiksmes
+// 7. sukurti nauja masyva kuriame turetume masyva su objektais
+// [
+//   {value: -1},
+//   {value: 5},
+//   {value: 8}
+//   {value: -20}
+//   ...
+// ]
